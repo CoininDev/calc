835043a110b061694953bfa53ad40708d96bde5f
gaybe <gabr.batz@gmail.com>
2023-01-12 06:23:13 -0300

    completo!!

--- calc.js
+++ calc.js
@@ -1,3 +1,20 @@
-function write_key(){
-    
+let input = ""
+
+let calc_screen = document.getElementById('screen-text')
+
+
+function write_key(key){
+    input += key
+    calc_screen.innerHTML = input
+}
+
+function clear_screen(){
+    input = ""
+    calc_screen.innerHTML = "0"
+}
+
+function calculate(){
+    output = eval(input)
+    input = ""
+    calc_screen.innerHTML = output
 }
